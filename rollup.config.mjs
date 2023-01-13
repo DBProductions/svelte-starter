1545509177cfa95c234d2637a35a4c1a3dddf969
DBCodes <dennis@dbproductions.de>
2023-01-13 23:13:24 +0100

    changed terser plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 
 const production = !process.env.ROLLUP_WATCH
 
